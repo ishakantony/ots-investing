da431645f390980ed49b8de54610273044d0c2fb
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2024-05-22 13:36:23 +0800

    Add Hono and navigation

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import {UserButton} from "@clerk/nextjs";
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">This is an authenticated route!</h1>
-        <UserButton />
-      <Button variant="default">Click Me!</Button>
-    </main>
-  );
+  return <p>Dashboard Page</p>
 }
